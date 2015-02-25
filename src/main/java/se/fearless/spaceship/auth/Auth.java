ce1a05a97dbe6a34b1e1be943a32954ccf002bd3
hiflyer <per.malmen@gmail.com>
2015-02-25 22:49:52 +0100

    Added a main method for simple authentication

---
+++ src/main/java/se/fearless/spaceship/auth/Auth.java
@@ -0,0 +1,25 @@
+package se.fearless.spaceship.auth;
+
+import se.fearless.service.MicroService;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class Auth {
+	public static void main(String[] args) {
+		final Set<String> userNames = new CopyOnWriteArraySet<String>(Arrays.asList("hiflyer", "demazia"));
+
+		MicroService microService = new MicroService(9999, (request, response) -> {
+			String userName = request.getPath().substring(1);
+			if (userNames.contains(userName)) {
+				response.writeString("SUCCESS");
+			} else {
+				response.writeString("FAIL");
+			}
+			return response.close();
+
+		});
+		microService.start();
+	}
+}
