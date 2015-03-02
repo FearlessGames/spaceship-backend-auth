6043f7fbbb0065ab702fdb1b0c1fa3d825be06e4
hiflyer <per.malmen@gmail.com>
2015-03-02 14:55:37 +0100

    The auth now accepts all user names

--- src/main/java/se/fearless/spaceship/auth/Auth.java
+++ src/main/java/se/fearless/spaceship/auth/Auth.java
@@ -7,13 +7,14 @@ import se.fearless.service.Router;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.regex.Pattern;
 
 public class Auth {
 	public static void main(String[] args) {
 		final Set<String> userNames = new CopyOnWriteArraySet<>(Arrays.asList("hiflyer", "demazia"));
 
 		Router router = new Router();
-		router.addRoute(HttpMethod.GET, "/*", (request, response) -> {
+		router.addRoute(HttpMethod.GET, Pattern.compile("/.*"), (request, response) -> {
 			String userName = request.getPath().substring(1);
 			if (userNames.contains(userName)) {
 				response.writeString("SUCCESS");
