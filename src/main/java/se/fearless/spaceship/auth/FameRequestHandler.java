1200be3fdc07f22e28758be8f6a3bd7b8cd7a383
hiflyer <per.malmen@gmail.com>
2015-05-13 16:12:12 +0200

    Changed from Charset.forName to StandardCharsets

--- src/main/java/se/fearless/spaceship/auth/FameRequestHandler.java
+++ src/main/java/se/fearless/spaceship/auth/FameRequestHandler.java
@@ -10,7 +10,7 @@ import io.reactivex.netty.protocol.http.server.RequestHandler;
 import rx.Observable;
 import se.fearless.common.json.JsonSerializer;
 
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 public class FameRequestHandler implements RequestHandler<ByteBuf, ByteBuf> {
@@ -28,7 +28,7 @@ public class FameRequestHandler implements RequestHandler<ByteBuf, ByteBuf> {
 		Observable<HttpClientResponse<ByteBuf>> httpGet = RxNetty.createHttpGet(server + "/api/private/users/spaceship/" + fameToken);
 
 		Observable<ByteBuf> byteBufObservable = httpGet.flatMap(AbstractHttpContentHolder::getContent);
-		Observable<String> stringObservable = byteBufObservable.map(byteBuf1 -> byteBuf1.toString(Charset.forName("UTF-8")));
+		Observable<String> stringObservable = byteBufObservable.map(byteBuf1 -> byteBuf1.toString(StandardCharsets.UTF_8));
 
 		Observable<UserAccountDTO> userAccountDTOObservable = stringObservable.map(s -> jsonSerializer.fromJson(s, UserAccountDTO.class));
 
